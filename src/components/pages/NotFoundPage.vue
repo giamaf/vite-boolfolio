4926d1cf463a20aaca02a0a3b00f9fa4998cb102
giamaf <gianluca.maffucci@hotmail.com>
2024-04-02 17:44:39 +0200

    added NotFoundPage_2

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+
+};
+</script>
+
+<template>
+    <div class="d-flex align-items-center justify-content-center" style=" height: calc(100vh - 60px);">
+        <h3 class="text-white">404 | Page not found</h3>
+    </div>
+
+</template>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,16 @@ import { createRouter, createWebHistory } from "vue-router";
 // Importo le pagine
 import HomePage from '../components/pages/HomePage.vue';
 import ContactUsPage from '../components/pages/ContactUsPage.vue';
+import NotFoundPage from '../components/pages/NotFoundPage.vue';
 
 // Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: HomePage },
-        { path: '/contact-us', component: ContactUsPage },
+        { path: '/', component: HomePage, name: 'Home' },
+        { path: '/contact-us', component: ContactUsPage, name: 'ContactUs' },
         // Qualunque path non incluso reindirizza alla HomePage anche se c'è qualcosa in query string
-        { path: '/:pathMatch(.*)*', redirect: '/' }
+        { path: '/:pathMatch(.*)*', component: NotFoundPage, name: 'NotFound' }
     ]
 
 })
