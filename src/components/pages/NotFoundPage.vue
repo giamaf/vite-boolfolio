7412785c8aea833a9c5df1fc7be2709413899a84
giamaf <gianluca.maffucci@hotmail.com>
2024-04-02 18:08:13 +0200

    added improvements

--- src/components/pages/NotFoundPage.vue
+++ src/components/pages/NotFoundPage.vue
@@ -6,8 +6,10 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex align-items-center justify-content-center" style=" height: calc(100vh - 60px);">
+    <div class="d-flex flex-column align-items-center justify-content-center" style=" height: calc(100vh - 60px);">
         <h3 class="text-white">404 | Page not found</h3>
+        <RouterLink to="/">Torna alla Home</RouterLink>
+        <a type="button" class="badge" @click="$router.back()">Torna indietro</a>
     </div>
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,13 @@ import NotFoundPage from '../components/pages/NotFoundPage.vue';
 // Definisco le rotte
 const router = createRouter({
     history: createWebHistory(),
+    linkExactActiveClass: 'active',
     routes: [
         { path: '/', component: HomePage, name: 'Home' },
         { path: '/contact-us', component: ContactUsPage, name: 'ContactUs' },
+        { path: '/not-found', component: NotFoundPage, name: 'NotFound' },
         // Qualunque path non incluso reindirizza alla HomePage anche se c'è qualcosa in query string
-        { path: '/:pathMatch(.*)*', component: NotFoundPage, name: 'NotFound' }
+        { path: '/:pathMatch(.*)*', redirect: 'not-found' }
     ]
 
 })
